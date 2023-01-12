a1f0fccce9abff9c197ab66fcd226563d2bc0b75
XavierDupuis <xavier0978@hotmail.fr>
2023-01-12 11:08:35 -0500

    add navHostController and pages

--- TP1/app/src/main/java/com/example/unblockme/MainActivity.kt
+++ TP1/app/src/main/java/com/example/unblockme/MainActivity.kt
@@ -6,11 +6,11 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.unblockme.common.view.Navigation
 import com.example.unblockme.menu.view.Menu
 import com.example.unblockme.ui.theme.UnblockMeTheme
 
@@ -28,10 +28,9 @@ fun MainContent() {
     UnblockMeTheme {
         Surface(
             modifier = Modifier.fillMaxSize(),
-
             color = MaterialTheme.colors.background
         ) {
-            Menu()
+            Navigation()
         }
     }
 }
