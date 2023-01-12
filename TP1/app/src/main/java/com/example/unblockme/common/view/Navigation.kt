e3ee37573f733cc71f39e628d14d658367942e91
XavierDupuis <xavier0978@hotmail.fr>
2023-01-12 11:32:43 -0500

    update nav

--- TP1/app/src/main/java/com/example/unblockme/common/view/Navigation.kt
+++ TP1/app/src/main/java/com/example/unblockme/common/view/Navigation.kt
@@ -5,6 +5,8 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.unblockme.about.view.About
+import com.example.unblockme.game.view.Game
 import com.example.unblockme.menu.view.Menu
 
 @Composable
