469ad394a7805aa324d98239537edbdc9fd54f73
XavierDupuis <xavier0978@hotmail.fr>
2023-01-17 20:37:24 -0500

    added level 1 complete layout

--- TP1/app/src/main/java/com/example/unblockme/game/domain/LevelLayout.kt
+++ TP1/app/src/main/java/com/example/unblockme/game/domain/LevelLayout.kt
@@ -1,13 +1,10 @@
 package com.example.unblockme.game.domain
 
-import com.example.unblockme.game.models.Block
-import com.example.unblockme.game.models.Blocks
-import com.example.unblockme.game.models.Direction
-import com.example.unblockme.game.models.Coordinates
+import com.example.unblockme.game.models.*
 
 
 val Level1Layout: Blocks = listOf(
-    Block(
+    OtherBlock(
         listOf(
             Coordinates(0, 0),
             Coordinates(1, 0),
@@ -15,7 +12,58 @@ val Level1Layout: Blocks = listOf(
         ),
         Direction.Horizontal
     ),
-    // TODO
+    OtherBlock(
+        listOf(
+            Coordinates(2, 1),
+            Coordinates(2, 2),
+            Coordinates(2, 3),
+        ),
+        Direction.Vertical
+    ),
+    MainBlock(
+        listOf(
+            Coordinates(0, 2),
+            Coordinates(1, 2),
+        ),
+        Direction.Horizontal
+    ),
+    OtherBlock(
+        listOf(
+            Coordinates(0, 3),
+            Coordinates(0, 4),
+        ),
+        Direction.Vertical
+    ),
+    OtherBlock(
+        listOf(
+            Coordinates(0, 5),
+            Coordinates(1, 5),
+            Coordinates(2, 5),
+        ),
+        Direction.Horizontal
+    ),
+    OtherBlock(
+        listOf(
+            Coordinates(5, 0),
+            Coordinates(5, 1),
+            Coordinates(5, 2),
+        ),
+        Direction.Vertical
+    ),
+    OtherBlock(
+        listOf(
+            Coordinates(4, 3),
+            Coordinates(5, 3),
+        ),
+        Direction.Horizontal
+    ),
+    OtherBlock(
+        listOf(
+            Coordinates(4, 4),
+            Coordinates(4, 5),
+        ),
+        Direction.Vertical
+    )
 )
 
 val Level2Layout: Blocks = listOf(
