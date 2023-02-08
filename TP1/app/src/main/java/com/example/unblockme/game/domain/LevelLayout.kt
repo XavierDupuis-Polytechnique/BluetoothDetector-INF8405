c57ac68b947eab59d14149f431d8a0349769aa2e
XavierDupuis <xavier0978@hotmail.fr>
2023-02-08 18:44:45 -0500

    layouts 2 & 3

--- TP1/app/src/main/java/com/example/unblockme/game/domain/LevelLayout.kt
+++ TP1/app/src/main/java/com/example/unblockme/game/domain/LevelLayout.kt
@@ -67,24 +67,116 @@ val Level1Layout: Blocks = listOf(
 )
 
 val Level2Layout: Blocks = listOf(
-    // TODO
+    MainBlock(
+        listOf(
+            Coordinates(0, 2),
+            Coordinates(1, 2),
+        ),
+        Direction.Horizontal
+    ),
     OtherBlock(
         listOf(
-            Coordinates(3, 0),
-            Coordinates(3, 1),
+            Coordinates(0, 3),
+            Coordinates(1, 3),
+        ),
+        Direction.Horizontal
+    ),
+    OtherBlock(
+        listOf(
+            Coordinates(2, 1),
+            Coordinates(2, 2),
+        ),
+        Direction.Vertical
+    ),
+    OtherBlock(
+        listOf(
+            Coordinates(2, 3),
+            Coordinates(2, 4),
+        ),
+        Direction.Vertical
+    ),
+    OtherBlock(
+        listOf(
+            Coordinates(2, 5),
+            Coordinates(3, 5),
+        ),
+        Direction.Horizontal
+    ),
+    OtherBlock(
+        listOf(
             Coordinates(3, 2),
+            Coordinates(3, 3),
+            Coordinates(3, 4),
+        ),
+        Direction.Vertical
+    ),
+    OtherBlock(
+        listOf(
+            Coordinates(4, 2),
+            Coordinates(4, 3),
+            Coordinates(4, 4),
         ),
         Direction.Vertical
     ),
 )
 
 val Level3Layout: Blocks = listOf(
-    // TODO
+    MainBlock(
+        listOf(
+            Coordinates(0, 2),
+            Coordinates(1, 2),
+        ),
+        Direction.Horizontal
+    ),
     OtherBlock(
         listOf(
+            Coordinates(0, 0),
+            Coordinates(0, 1),
+        ),
+        Direction.Vertical
+    ),
+    OtherBlock(
+        listOf(
+            Coordinates(0, 4),
+            Coordinates(1, 4),
+            Coordinates(2, 4),
+        ),
+        Direction.Horizontal
+    ),
+    OtherBlock(
+        listOf(
+            Coordinates(1, 0),
+            Coordinates(2, 0),
+        ),
+        Direction.Horizontal
+    ),
+    OtherBlock(
+        listOf(
+            Coordinates(3, 0),
             Coordinates(4, 0),
-            Coordinates(4, 1),
+        ),
+        Direction.Horizontal
+    ),
+    OtherBlock(
+        listOf(
+            Coordinates(2, 1),
+            Coordinates(2, 2),
+        ),
+        Direction.Vertical
+    ),
+    OtherBlock(
+        listOf(
+            Coordinates(3, 2),
+            Coordinates(3, 3),
+            Coordinates(3, 4),
+        ),
+        Direction.Vertical
+    ),
+    OtherBlock(
+        listOf(
             Coordinates(4, 2),
+            Coordinates(4, 3),
+            Coordinates(4, 4),
         ),
         Direction.Vertical
     ),
