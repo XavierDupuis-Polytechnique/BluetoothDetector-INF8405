fa8530cd2b596f577ba6b95749ada9c3caf0cdd0
XavierDupuis <xavier0978@hotmail.fr>
2023-02-15 16:09:20 -0500

    revert level 1 commented

--- TP1/app/src/main/java/com/example/unblockme/game/domain/LevelLayout.kt
+++ TP1/app/src/main/java/com/example/unblockme/game/domain/LevelLayout.kt
@@ -4,22 +4,22 @@ import com.example.unblockme.game.models.*
 
 // Level layout for level 1
 val Level1Layout: Blocks = listOf(
-//    OtherBlock(
-//        listOf(
-//            Coordinates(0, 0),
-//            Coordinates(1, 0),
-//            Coordinates(2, 0),
-//        ),
-//        Direction.Horizontal
-//    ),
-//    OtherBlock(
-//        listOf(
-//            Coordinates(2, 1),
-//            Coordinates(2, 2),
-//            Coordinates(2, 3),
-//        ),
-//        Direction.Vertical
-//    ),
+    OtherBlock(
+        listOf(
+            Coordinates(0, 0),
+            Coordinates(1, 0),
+            Coordinates(2, 0),
+        ),
+        Direction.Horizontal
+    ),
+    OtherBlock(
+        listOf(
+            Coordinates(2, 1),
+            Coordinates(2, 2),
+            Coordinates(2, 3),
+        ),
+        Direction.Vertical
+    ),
     MainBlock(
         listOf(
             Coordinates(0, 2),
@@ -27,43 +27,43 @@ val Level1Layout: Blocks = listOf(
         ),
         Direction.Horizontal
     ),
-//    OtherBlock(
-//        listOf(
-//            Coordinates(0, 3),
-//            Coordinates(0, 4),
-//        ),
-//        Direction.Vertical
-//    ),
-//    OtherBlock(
-//        listOf(
-//            Coordinates(0, 5),
-//            Coordinates(1, 5),
-//            Coordinates(2, 5),
-//        ),
-//        Direction.Horizontal
-//    ),
-//    OtherBlock(
-//        listOf(
-//            Coordinates(5, 0),
-//            Coordinates(5, 1),
-//            Coordinates(5, 2),
-//        ),
-//        Direction.Vertical
-//    ),
-//    OtherBlock(
-//        listOf(
-//            Coordinates(4, 3),
-//            Coordinates(5, 3),
-//        ),
-//        Direction.Horizontal
-//    ),
-//    OtherBlock(
-//        listOf(
-//            Coordinates(4, 4),
-//            Coordinates(4, 5),
-//        ),
-//        Direction.Vertical
-//    )
+    OtherBlock(
+        listOf(
+            Coordinates(0, 3),
+            Coordinates(0, 4),
+        ),
+        Direction.Vertical
+    ),
+    OtherBlock(
+        listOf(
+            Coordinates(0, 5),
+            Coordinates(1, 5),
+            Coordinates(2, 5),
+        ),
+        Direction.Horizontal
+    ),
+    OtherBlock(
+        listOf(
+            Coordinates(5, 0),
+            Coordinates(5, 1),
+            Coordinates(5, 2),
+        ),
+        Direction.Vertical
+    ),
+    OtherBlock(
+        listOf(
+            Coordinates(4, 3),
+            Coordinates(5, 3),
+        ),
+        Direction.Horizontal
+    ),
+    OtherBlock(
+        listOf(
+            Coordinates(4, 4),
+            Coordinates(4, 5),
+        ),
+        Direction.Vertical
+    )
 )
 
 // Level layout for level 2
