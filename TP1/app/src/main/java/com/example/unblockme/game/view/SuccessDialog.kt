caeaf895845f6dc50ab1f1ba62c47ae45a8881b1
medimad31 <122495066+medimad31@users.noreply.github.com>
2023-02-15 18:27:25 -0600

    Update SuccessDialog.kt

--- TP1/app/src/main/java/com/example/unblockme/game/view/SuccessDialog.kt
+++ TP1/app/src/main/java/com/example/unblockme/game/view/SuccessDialog.kt
@@ -1,24 +1,48 @@
 package com.example.unblockme.game.view
 
 import android.media.MediaPlayer
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.AnimatedVisibilityScope
+import androidx.compose.animation.ExitTransition
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.unblockme.R
 import com.example.unblockme.common.view.CustomDialog
 import com.example.unblockme.game.viewmodel.SuccessViewModel
+import androidx.compose.animation.AnimatedVisibility
 
 @Composable
 fun SuccessDialog(
     viewModel: SuccessViewModel = viewModel()
-) {
-    if (viewModel.isSuccessShown.value) {
+
+){
+    AnimatedVisibility(visible = viewModel.isSuccessShown.value,exit = fadeOut(animationSpec = tween(10000, easing = LinearEasing)))
+    {
+
+    }
+
+
+
+
         //The success window will appear with sound
         val context = LocalContext.current
         val mediaPlayer: MediaPlayer = MediaPlayer.create(context, R.raw.audio)
         mediaPlayer.start()
+
         CustomDialog(
             onDismiss = { viewModel.onDismissDialog() }
         )
-    }
+
 }
+
+
