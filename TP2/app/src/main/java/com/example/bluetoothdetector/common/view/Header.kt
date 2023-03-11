c2b72d34b8e5b9de815013b6c60dab4445d1d82f
XavierDupuis <xavier0978@hotmail.fr>
2023-03-10 21:26:19 -0500

    proper spacing

--- TP2/app/src/main/java/com/example/bluetoothdetector/common/view/Header.kt
+++ TP2/app/src/main/java/com/example/bluetoothdetector/common/view/Header.kt
@@ -1,6 +1,10 @@
 package com.example.bluetoothdetector.common.view
 
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.bluetoothdetector.common.view.permissions.PermissionHeader
 import com.example.bluetoothdetector.common.viewmodel.PermissionsViewModel
 import com.example.bluetoothdetector.common.viewmodel.ThemeSelectorViewModel
@@ -10,8 +14,11 @@ fun HeaderView(
     themeSelectorViewModel: ThemeSelectorViewModel,
     permissionsViewModel: PermissionsViewModel
 ) {
-    CenteredHorizontalContainer {
+    CenteredHorizontalContainer(
+        modifier = Modifier.padding(horizontal = 12.dp)
+    ) {
         PermissionHeader(permissionsViewModel)
+        Spacer(modifier = Modifier.weight(1f))
         ThemeSelector(themeSelectorViewModel)
     }
 }
