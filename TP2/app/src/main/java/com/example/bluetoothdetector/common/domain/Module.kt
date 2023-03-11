d73dd8efbb9c375cf0e04af065328407cf4d4c2b
XavierDupuis <xavier0978@hotmail.fr>
2023-03-10 21:57:31 -0500

    update text

--- TP2/app/src/main/java/com/example/bluetoothdetector/common/domain/Module.kt
+++ TP2/app/src/main/java/com/example/bluetoothdetector/common/domain/Module.kt
@@ -1,6 +1,8 @@
 package com.example.bluetoothdetector.common.domain
 
 import android.Manifest
+import com.example.bluetoothdetector.ui.theme.DETECTOR_MODULE_DESCRIPTION
+import com.example.bluetoothdetector.ui.theme.MAPS_MODULE_DESCRIPTION
 
 enum class ModuleType {
     MAPS,
@@ -9,14 +11,14 @@ enum class ModuleType {
 
 data class Module(
     val moduleType: ModuleType,
-    val description: String,
+    val description: String = moduleType.name,
     val permissions: Permissions = listOf()
 )
 
 val Modules = listOf(
     Module(
         ModuleType.MAPS,
-        "ModuleType.MAPS",
+        MAPS_MODULE_DESCRIPTION,
         listOf(
             Manifest.permission.ACCESS_COARSE_LOCATION,
             Manifest.permission.ACCESS_FINE_LOCATION
@@ -24,10 +26,11 @@ val Modules = listOf(
     ),
     Module(
         ModuleType.DETECTOR,
-        "ModuleType.DETECTOR",
+        DETECTOR_MODULE_DESCRIPTION,
         listOf(
             Manifest.permission.BLUETOOTH,
             Manifest.permission.BLUETOOTH_ADMIN,
+            // TODO : INVESTIGATE -> ADD / REMOVE
             // https://developer.android.com/guide/topics/connectivity/bluetooth/permissions
             // Manifest.permission.ACCESS_BACKGROUND_LOCATION
             // Manifest.permission.BLUETOOTH_CONNECT,
