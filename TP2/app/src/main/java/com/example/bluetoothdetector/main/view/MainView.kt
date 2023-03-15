cd7f6cfae67bcc1d2a27f1acb38c59c2670c27f0
XavierDupuis <xavier0978@hotmail.fr>
2023-03-14 22:17:55 -0400

    panels weights

--- TP2/app/src/main/java/com/example/bluetoothdetector/main/view/MainView.kt
+++ TP2/app/src/main/java/com/example/bluetoothdetector/main/view/MainView.kt
@@ -3,8 +3,6 @@ package com.example.bluetoothdetector.main.view
 import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
@@ -12,14 +10,16 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.bluetoothdetector.common.view.CardContainer
-import com.example.bluetoothdetector.common.view.CenteredHorizontalContainer
+import com.example.bluetoothdetector.common.view.containers.CardContainer
+import com.example.bluetoothdetector.common.view.containers.CenteredHorizontalContainer
 import com.example.bluetoothdetector.common.view.Page
 import com.example.bluetoothdetector.main.sources.LocationSource
 import com.example.bluetoothdetector.main.viewmodel.MainViewModel
 import com.example.bluetoothdetector.ui.theme.BluetoothDetectorTheme
-import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
+
+const val RightWeight = 1f
+const val LeftWeight = RightWeight * 3
 
 @Composable
 fun MainScreen(
@@ -33,12 +33,16 @@ fun MainScreen(
     ) {
         CardContainer(
             modifier = Modifier
-                .weight(0.75f)
+                .weight(LeftWeight)
                 .fillMaxHeight()
         ) {
             MapView(locationSource)
         }
-        CardContainer {
+        CardContainer(
+            modifier = Modifier
+                .weight(RightWeight)
+                .fillMaxHeight()
+        ) {
             DevicesListView()
         }
     }
