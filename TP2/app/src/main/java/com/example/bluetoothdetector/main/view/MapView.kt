e8c1f70537f0ec57463823042dae6b0f2969dad7
XavierDupuis <xavier0978@hotmail.fr>
2023-03-27 10:15:19 -0400

    map example

--- TP2/app/src/main/java/com/example/bluetoothdetector/main/view/MapView.kt
+++ TP2/app/src/main/java/com/example/bluetoothdetector/main/view/MapView.kt
@@ -1,44 +1,45 @@
 package com.example.bluetoothdetector.main.view
 
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.bluetoothdetector.common.view.containers.CenteredVerticalContainer
 import com.example.bluetoothdetector.main.viewmodel.MapViewModel
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerState
+import com.google.maps.android.compose.rememberCameraPositionState
 
 @Composable
 fun MapView(
     viewModel: MapViewModel = hiltViewModel()
 ) {
-    val longitude = viewModel.location.value?.longitude.toString()
-    val latitude = viewModel.location.value?.latitude.toString()
-    // TODO
     CenteredVerticalContainer {
-        GoogleMap()
+        // TODO
+        val singapore = LatLng(1.35, 103.87)
+        val cameraPositionState = rememberCameraPositionState {
+            position = CameraPosition.fromLatLngZoom(singapore, 10f)
+        }
+        GoogleMap(
+            modifier = Modifier.fillMaxSize(),
+            cameraPositionState = cameraPositionState
+        ) {
+            Marker(
+                state = MarkerState(position = singapore),
+                title = "Singapore",
+                snippet = "Marker in Singapore"
+            )
+        }
+
+        // TODO : REMOVE NEXT LINES
+        val longitude = viewModel.location.value?.longitude.toString()
+        val latitude = viewModel.location.value?.latitude.toString()
         Text("Map")
         Text("longitude : $longitude")
         Text("latitude : $latitude")
     }
-}
-
-@Composable()
-fun GoogleMap()
-/*modifier: Modifier = Modifier,
-              cameraPositionState: CameraPositionState = rememberCameraPositionState(),
-              contentDescription: String? = null,
-              googleMapOptionsFactory: () -> GoogleMapOptions = { GoogleMapOptions() },
-              properties: MapProperties = DefaultMapProperties,
-              locationSource: LocationSource? = null,
-              uiSettings: MapUiSettings = DefaultMapUiSettings,
-              indoorStateChangeListener: IndoorStateChangeListener = DefaultIndoorStateChangeListener,
-              onMapClick: (LatLng) -> Unit = {},
-              onMapLongClick: (LatLng) -> Unit = {},
-              onMapLoaded: () -> Unit = {},
-              onMyLocationButtonClick: () -> Boolean = { false },
-              onMyLocationClick: (Location) -> Unit = {},
-              onPOIClick: (PointOfInterest) -> Unit = {},
-              contentPadding: PaddingValues = NoPadding,
-              content: @Composable() () -> Unit? = null)*/
-{
-
 }
