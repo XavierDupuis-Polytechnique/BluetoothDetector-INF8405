6c2eaabbf1fc88a9874ae2aeac9474222520e5ac
XavierDupuis <xavier0978@hotmail.fr>
2023-03-29 17:36:57 -0400

    remove unused import

--- TP2/app/src/main/java/com/example/bluetoothdetector/main/repository/DeviceRepository.kt
+++ TP2/app/src/main/java/com/example/bluetoothdetector/main/repository/DeviceRepository.kt
@@ -1,7 +1,6 @@
 package com.example.bluetoothdetector.main.repository
 
 import androidx.compose.runtime.mutableStateMapOf
-import androidx.compose.runtime.mutableStateOf
 import com.example.bluetoothdetector.main.model.Device
 import com.example.bluetoothdetector.main.sources.DeviceSource
 import javax.inject.Inject
