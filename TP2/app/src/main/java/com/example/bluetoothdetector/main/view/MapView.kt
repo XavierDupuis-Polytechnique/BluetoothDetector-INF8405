a1c6ef5b2d085ab981b4ede45223f775cd278d3c
XavierDupuis <xavier0978@hotmail.fr>
2023-03-29 22:36:07 -0400

    Update MapView.kt

--- TP2/app/src/main/java/com/example/bluetoothdetector/main/view/MapView.kt
+++ TP2/app/src/main/java/com/example/bluetoothdetector/main/view/MapView.kt
@@ -35,13 +35,19 @@ fun MapView(
                 icon = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE),
 
             )
+
             viewModel.devices.values.forEach {
-                //if(it.location?.value?.latitude != null && it.location?.value?.longitude!=null)
-                Marker(
-                    state = MarkerState(position = LatLng(it.location!!.latitude,it.location!!.longitude)),
-                    title = "${it.name}",
-                    /*snippet = "Marker in Singapore"*///text under the title when click on marker
-                )
+                it.location?.let { location ->
+                    Marker(
+                        state = MarkerState(
+                            position = LatLng(
+                                location.latitude,
+                                location.longitude
+                            )
+                        ),
+                        title = it.name,
+                    )
+                }
             }
         }
 
