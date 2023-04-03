8ac66afec0dd78fd6d86e8688c19f02576fc7779
XavierDupuis <xavier0978@hotmail.fr>
2023-04-02 21:29:12 -0400

    bump db schema version

--- TP2/app/src/main/java/com/example/bluetoothdetector/main/sources/DeviceSource.kt
+++ TP2/app/src/main/java/com/example/bluetoothdetector/main/sources/DeviceSource.kt
@@ -7,7 +7,7 @@ import com.example.bluetoothdetector.main.domain.DeviceConverter
 import com.example.bluetoothdetector.main.domain.DeviceDao
 import com.example.bluetoothdetector.main.model.Device
 
-@Database(entities = [Device::class], version = 2)
+@Database(entities = [Device::class], version = 4)
 @TypeConverters(DeviceConverter::class)
 abstract class DeviceSource : RoomDatabase() {
 
