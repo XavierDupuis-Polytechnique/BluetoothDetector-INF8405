6838ee7b012b6667d537e133f108fa49066133ea
XavierDupuis <xavier0978@hotmail.fr>
2023-04-02 23:15:13 -0400

    move intents in DeviceRepository

--- TP2/app/src/main/java/com/example/bluetoothdetector/main/repository/DeviceRepository.kt
+++ TP2/app/src/main/java/com/example/bluetoothdetector/main/repository/DeviceRepository.kt
@@ -2,6 +2,8 @@ package com.example.bluetoothdetector.main.repository
 
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
+import android.widget.Toast
 import androidx.compose.runtime.mutableStateMapOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.core.content.ContextCompat.startActivity
@@ -55,13 +57,33 @@ class DeviceRepository @Inject constructor(
     val highlightedDevice = mutableStateOf<Device?>(null)
 
     fun share(device: Device) {
-        val shareIntent: Intent = Intent().apply {
+        val intent: Intent = Intent().apply {
             action = Intent.ACTION_SEND
-            // TODO : ADD OTHER INFORMATION
-            putExtra(Intent.EXTRA_TEXT, device.name)
+            putExtra(Intent.EXTRA_TEXT, device.toString())
             type = "text/plain"
         }
-        startActivity(context, shareIntent, null)
+        try {
+            startActivity(context, intent, null)
+        } catch (exception: Exception) {
+            Toast.makeText(context, "Could not share device", Toast.LENGTH_SHORT).show()
+            exception.printStackTrace()
+        }
+    }
+
+    fun getItinerary(device: Device, zoom: Int = 18) {
+        device.location?.let {
+            // https://developers.google.com/maps/documentation/urls/android-intents
+            val intent = Intent(
+                Intent.ACTION_VIEW,
+                Uri.parse("geo:0,0?q=${it.latitude},${it.longitude} (${device.name})")
+            )
+            try {
+                startActivity(context, intent, null)
+            } catch (exception: Exception) {
+                Toast.makeText(context, "Google Maps is not installed or is disabled", Toast.LENGTH_SHORT).show()
+                exception.printStackTrace()
+            }
+        }
     }
 
     fun forgetDevice(device: Device) {
@@ -112,10 +134,6 @@ class DeviceRepository @Inject constructor(
             println("COULD NOT EXECUTE $operation")
             exception.printStackTrace()
         }
-    }
-
-    fun getItinerary(device: Device) {
-        deviceSource.getItinerary(device)
     }
 
     fun isFavorite(device: Device): Boolean {
