8597e4c055e2960c246ae9608ec9f0829cf37b3e
XavierDupuis <xavier0978@hotmail.fr>
2023-04-02 20:09:08 -0400

    Update DeviceConverter.kt

--- TP2/app/src/main/java/com/example/bluetoothdetector/main/domain/DeviceConverter.kt
+++ TP2/app/src/main/java/com/example/bluetoothdetector/main/domain/DeviceConverter.kt
@@ -8,32 +8,26 @@ import java.util.*
 // https://developer.android.com/training/data-storage/room/referencing-data
 class DeviceConverter : AbstractConverter() {
     @TypeConverter
-    fun dateFromTimestamp(value: String?): Date? {
-        return decode(value, Date::class.java)
-    }
+    fun dateFromTimestamp(value: String?): Date? =
+        decode(value, Date::class.java)
 
     @TypeConverter
-    fun dateToTimestamp(value: Date?): String? {
-        return encode(value)
-    }
+    fun dateToTimestamp(value: Date?): String? =
+        encode(value)
 
     @TypeConverter
-    fun locationFromString(value: String?): Location? {
-        return decode(value, Location::class.java)
-    }
+    fun locationFromString(value: String?): Location? =
+        decode(value, Location::class.java)
 
     @TypeConverter
-    fun locationToString(value: Location?): String? {
-        return encode(value)
-    }
+    fun locationToString(value: Location?): String? =
+        encode(value)
 
     @TypeConverter
-    fun parcelUuidsFromString(value: String?): List<ParcelUuid>? {
-        return decode(value, Array<ParcelUuid>::class.java)
-    }
+    fun parcelUuidsFromString(value: String?): List<ParcelUuid>? =
+        decode(value, Array<ParcelUuid>::class.java)
 
     @TypeConverter
-    fun parcelUuidsToString(value: List<ParcelUuid>?): String? {
-        return encode(value)
-    }
+    fun parcelUuidsToString(value: List<ParcelUuid>?): String? =
+        encode(value)
 }
