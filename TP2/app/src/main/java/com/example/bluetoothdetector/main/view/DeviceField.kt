59d1666e01ffd0b3077981d6fa6070cc904ab148
XavierDupuis <xavier0978@hotmail.fr>
2023-04-02 21:54:04 -0400

    device view named fields

---
+++ TP2/app/src/main/java/com/example/bluetoothdetector/main/view/DeviceField.kt
@@ -0,0 +1,22 @@
+package com.example.bluetoothdetector.main.view
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.font.FontWeight
+import com.example.bluetoothdetector.common.view.containers.CenteredHorizontalContainer
+
+@Composable
+fun DeviceField(
+    deviceFieldValue: String,
+    deviceFieldName: String? = null
+) {
+    CenteredHorizontalContainer {
+        deviceFieldName?.let {
+            Text("$deviceFieldName : ")
+        }
+        Text(
+            fontWeight = FontWeight.Bold,
+            text = deviceFieldValue
+        )
+    }
+}
