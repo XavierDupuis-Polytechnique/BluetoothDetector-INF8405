afeedac63180a2653fe6011e1bfae561f1686258
XavierDupuis <xavier0978@hotmail.fr>
2023-04-05 22:58:34 -0400

    update splashscreen version

--- Projet/app/src/main/java/com/example/bluetoothdetector/splash/view/Splash.kt
+++ Projet/app/src/main/java/com/example/bluetoothdetector/splash/view/Splash.kt
@@ -11,6 +11,7 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.bluetoothdetector.BuildConfig
 import com.example.bluetoothdetector.R
 import com.example.bluetoothdetector.common.view.Page
 import com.example.bluetoothdetector.common.view.SpinnerView
@@ -19,7 +20,7 @@ import com.example.bluetoothdetector.common.view.containers.CenteredHorizontalCo
 import com.example.bluetoothdetector.common.view.containers.CenteredVerticalContainer
 import com.example.bluetoothdetector.splash.viewmodel.SplashViewModel
 
-const val AppVersion = 1.0
+const val AppVersion = BuildConfig.VERSION_NAME
 const val Logo = R.drawable.icon
 val LogoSize = 36.dp
 val Developers = listOf(
