0927fed5f7159e24147eed57d5316159f2ec1581
XavierDupuis <xavier0978@hotmail.fr>
2023-04-09 15:27:51 -0400

    SelectableListItem for language selection

--- Projet/app/src/main/java/com/example/bluetoothdetector/common/view/languages/LanguageModal.kt
+++ Projet/app/src/main/java/com/example/bluetoothdetector/common/view/languages/LanguageModal.kt
@@ -1,20 +1,13 @@
 package com.example.bluetoothdetector.common.view.languages
 
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import com.example.bluetoothdetector.R
 import com.example.bluetoothdetector.common.domain.action.Action
 import com.example.bluetoothdetector.common.domain.modal.ModalActions
 import com.example.bluetoothdetector.common.repository.LanguageRepository
-import com.example.bluetoothdetector.common.view.containers.CenteredHorizontalContainer
+import com.example.bluetoothdetector.common.view.SelectableListItem
 import com.example.bluetoothdetector.common.view.modal.Modal
 import com.example.bluetoothdetector.common.viewmodel.LanguageViewModel
 
@@ -39,18 +32,12 @@ fun LanguagesModal(
                         backgroundColor = MaterialTheme.colors.surface
                     )
 
-            Button(
-                // TODO : REVIEW UX
-                // enabled = !viewModel.isCurrentLanguage(it),
-                onClick = { viewModel.selectLanguage(it) },
-                colors = colors
-            ) {
-                CenteredHorizontalContainer {
-                    Text(stringResource(it.abbreviation))
-                    Spacer(modifier = Modifier.padding(8.dp))
-                    Text(stringResource(it.denomination))
-                }
-            }
+            SelectableListItem(
+                isSelected = { viewModel.isSelectedLanguage(it) },
+                select = { viewModel.selectLanguage(it) },
+                mainText = it.denomination,
+                icon = it.abbreviation
+            )
         }
         it(
             ModalActions(
--- Projet/app/src/main/java/com/example/bluetoothdetector/menu/view/MenuTabView.kt
+++ Projet/app/src/main/java/com/example/bluetoothdetector/menu/view/MenuTabView.kt
@@ -1,18 +1,13 @@
 package com.example.bluetoothdetector.menu.view
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.material.*
+import androidx.compose.material.DrawerState
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.navigation.NavHostController
 import com.example.bluetoothdetector.common.domain.Page
+import com.example.bluetoothdetector.common.view.SelectableListItem
 import com.example.bluetoothdetector.menu.viewmodel.MenuViewModel
 import kotlinx.coroutines.CoroutineScope
 
-@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun MenuTabView(
     viewModel: MenuViewModel,
@@ -21,37 +16,13 @@ fun MenuTabView(
     menuScope: CoroutineScope,
     page: Page,
 ) {
-    val color =
-        if (viewModel.isSelectedTab(page))
-            MaterialTheme.colors.secondary
-        else
-            MaterialTheme.colors.primary
-    val fontWeight =
-        if (viewModel.isSelectedTab(page))
-            FontWeight.Bold
-        else
-            null
-    ListItem(
-        modifier = Modifier
-            .clickable { viewModel.navigate(navController, menuState, menuScope, page) }
-            .background(color.copy(alpha = 0.2f)),
-        text = {
-            Text(
-                text = stringResource(page.denomination),
-                fontWeight = fontWeight
-            )
-        },
-        secondaryText = {
-            Text(
-                text = stringResource(page.description),
-                fontWeight = fontWeight
-            )
-        },
-        icon = {
-            Icon(
-                imageVector = page.icon,
-                contentDescription = page.icon.toString(),
-            )
-        }
+    val isSelected = { viewModel.isSelectedTab(page) }
+    val select = { viewModel.navigate(navController, menuState, menuScope, page) }
+    SelectableListItem(
+        isSelected = isSelected,
+        select = select,
+        mainText = page.denomination,
+        secondaryText = page.description,
+        icon = page.icon
     )
 }
