3e04147d72698c310a1eec7f1f30970d1794e4ff
XavierDupuis <xavier0978@hotmail.fr>
2023-04-09 14:37:58 -0400

    ux & colors for menu tabs

--- Projet/app/src/main/java/com/example/bluetoothdetector/menu/view/MenuTabView.kt
+++ Projet/app/src/main/java/com/example/bluetoothdetector/menu/view/MenuTabView.kt
@@ -1,10 +1,13 @@
 package com.example.bluetoothdetector.menu.view
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.navigation.NavHostController
 import com.example.bluetoothdetector.common.domain.Page
 import com.example.bluetoothdetector.menu.viewmodel.MenuViewModel
@@ -19,14 +22,32 @@ fun MenuTabView(
     menuScope: CoroutineScope,
     page: Page,
 ) {
+    val color =
+        if (viewModel.isSelectedTab(page))
+            MaterialTheme.colors.secondary
+        else
+            MaterialTheme.colors.primary
+    val fontWeight =
+        if (viewModel.isSelectedTab(page))
+            FontWeight.Bold
+        else
+            null
     ListItem(
-        modifier = Modifier.clickable { viewModel.navigate(navController, menuState, menuScope, page) },
-        text = { Text(stringResource(page.denomination)) },
-        secondaryText = { Text(stringResource(page.description)) },
+        modifier = Modifier
+            .clickable { viewModel.navigate(navController, menuState, menuScope, page) }
+            .background(color.copy(alpha = 0.2f)),
+        text = { Text(
+            text = stringResource(page.denomination),
+            fontWeight = fontWeight
+        ) },
+        secondaryText = { Text(
+            text = stringResource(page.description),
+            fontWeight = fontWeight
+        ) },
         icon = {
             Icon(
                 imageVector = page.icon,
-                contentDescription = page.icon.toString()
+                contentDescription = page.icon.toString(),
             )
         }
     )
