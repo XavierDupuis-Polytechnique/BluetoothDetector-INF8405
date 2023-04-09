c8502ea3a033d8594a4761139f97a26923ce6d33
XavierDupuis <xavier0978@hotmail.fr>
2023-04-09 13:41:36 -0400

    add menu button to header

--- Projet/app/src/main/java/com/example/bluetoothdetector/common/view/Header.kt
+++ Projet/app/src/main/java/com/example/bluetoothdetector/common/view/Header.kt
@@ -2,24 +2,25 @@ package com.example.bluetoothdetector.common.view
 
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.DrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavHostController
 import com.example.bluetoothdetector.common.view.containers.CenteredHorizontalContainer
 import com.example.bluetoothdetector.common.view.languages.LanguagesHeader
-import com.example.bluetoothdetector.common.view.permissions.PermissionHeader
-import com.example.bluetoothdetector.settings.view.SettingsHeader
+import com.example.bluetoothdetector.menu.view.MenuHeader
+import kotlinx.coroutines.CoroutineScope
 
 // Header container
 @Composable
 fun HeaderView(
-    navController: NavHostController,
+    menuState: DrawerState,
+    menuScope: CoroutineScope,
 ) {
     CenteredHorizontalContainer(
         modifier = Modifier.padding(horizontal = 12.dp)
     ) {
-        SettingsHeader(navController)
+        MenuHeader(menuState, menuScope)
         Spacer(modifier = Modifier.weight(1f))
         ThemeSelector()
         Spacer(modifier = Modifier.weight(1f))
