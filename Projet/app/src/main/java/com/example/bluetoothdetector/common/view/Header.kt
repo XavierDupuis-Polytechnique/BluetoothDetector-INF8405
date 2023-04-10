e9b766153eb72c658b6c0df6340dc8e6acabddc6
XavierDupuis <xavier0978@hotmail.fr>
2023-04-09 20:33:06 -0400

    add current page in header

--- Projet/app/src/main/java/com/example/bluetoothdetector/common/view/Header.kt
+++ Projet/app/src/main/java/com/example/bluetoothdetector/common/view/Header.kt
@@ -5,10 +5,14 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.DrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.bluetoothdetector.common.view.containers.CenteredHorizontalContainer
 import com.example.bluetoothdetector.common.view.languages.LanguagesHeader
+import com.example.bluetoothdetector.common.view.typography.Subtitle
 import com.example.bluetoothdetector.menu.view.MenuHeader
+import com.example.bluetoothdetector.menu.viewmodel.MenuViewModel
 import kotlinx.coroutines.CoroutineScope
 
 // Header container
@@ -17,10 +21,12 @@ fun HeaderView(
     menuState: DrawerState,
     menuScope: CoroutineScope,
 ) {
+    val viewModel: MenuViewModel = hiltViewModel()
     CenteredHorizontalContainer(
         modifier = Modifier.padding(horizontal = 12.dp)
     ) {
-        MenuHeader(menuState, menuScope)
+        MenuHeader(menuState, menuScope, viewModel)
+        Subtitle(stringResource(viewModel.selectedTab.value.denomination).uppercase())
         Spacer(modifier = Modifier.weight(1f))
         ThemeSelector()
         Spacer(modifier = Modifier.weight(1f))
