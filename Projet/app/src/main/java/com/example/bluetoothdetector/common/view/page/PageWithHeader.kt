83b7704429e44f86a42a532954071698410319ab
XavierDupuis <xavier0978@hotmail.fr>
2023-04-10 00:43:19 -0400

    extract page composables

--- Projet/app/src/main/java/com/example/bluetoothdetector/common/view/page/PageWithHeader.kt
+++ Projet/app/src/main/java/com/example/bluetoothdetector/common/view/page/PageWithHeader.kt
@@ -1,30 +1,15 @@
-package com.example.bluetoothdetector.common.view
+package com.example.bluetoothdetector.common.view.page
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Card
 import androidx.compose.material.DrawerState
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import com.example.bluetoothdetector.common.view.HeaderView
 import com.example.bluetoothdetector.common.view.containers.CenteredVerticalContainer
 import kotlinx.coroutines.CoroutineScope
-
-// Full page component for main views
-@Composable
-fun Page(content: @Composable () -> Unit) {
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(MaterialTheme.colors.surface),
-        contentAlignment = Alignment.Center,
-    ) {
-        content()
-    }
-}
 
 // Full page component for main views with theme selection
 @Composable
@@ -37,7 +22,7 @@ fun PageWithHeader(
     Page {
         CenteredVerticalContainer {
             Card(
-                modifier = Modifier.fillMaxWidth()
+                modifier = Modifier.fillMaxWidth(),
             ) {
                 headerContent()
             }
