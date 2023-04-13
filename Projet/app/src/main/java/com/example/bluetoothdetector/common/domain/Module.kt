d7b6c6183f58ece773d1b5384342b1adfc2cff44
XavierDupuis <xavier0978@hotmail.fr>
2023-04-12 20:25:28 -0400

    remove fine location

--- Projet/app/src/main/java/com/example/bluetoothdetector/common/domain/Module.kt
+++ Projet/app/src/main/java/com/example/bluetoothdetector/common/domain/Module.kt
@@ -22,7 +22,6 @@ val MapsModule = Module(
     R.string.maps_module_description,
     listOf(
         Manifest.permission.ACCESS_COARSE_LOCATION,
-        Manifest.permission.ACCESS_FINE_LOCATION
     )
 )
 
