d030226ae2173381f815b19e1058ab56d42e2d3b
XavierDupuis <xavier0978@hotmail.fr>
2023-04-13 13:29:08 -0400

    lint

--- Projet/app/src/main/java/com/example/bluetoothdetector/BluetoothDetector.kt
+++ Projet/app/src/main/java/com/example/bluetoothdetector/BluetoothDetector.kt
@@ -1,9 +1,6 @@
 package com.example.bluetoothdetector
 
 import android.app.Application
-import com.example.bluetoothdetector.common.Navigation
-import com.example.bluetoothdetector.login.LoginViewModel
-import com.example.bluetoothdetector.ui.theme.BluetoothDetectorTheme
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
