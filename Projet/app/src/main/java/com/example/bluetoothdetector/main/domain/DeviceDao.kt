726d00238488cac4c3c797049e0d222948ac3159
XavierDupuis <xavier0978@hotmail.fr>
2023-04-17 23:14:19 -0400

    CollectionDao as interface

--- Projet/app/src/main/java/com/example/bluetoothdetector/main/domain/DeviceDao.kt
+++ Projet/app/src/main/java/com/example/bluetoothdetector/main/domain/DeviceDao.kt
@@ -1,48 +1,31 @@
 package com.example.bluetoothdetector.main.domain
 
 import androidx.room.*
-import androidx.room.OnConflictStrategy.REPLACE
 import com.example.bluetoothdetector.main.model.Device
 import kotlinx.coroutines.flow.Flow
 
 
-// Offers database access through defined operations
+// Offers device database access through defined operations
 @Dao
-interface DeviceDao {
+interface DeviceDao : CollectionDao<Device> {
     @Query("SELECT * FROM devices")
     suspend fun getAll(): List<Device>
 
     @Query("SELECT * FROM devices")
     fun observeAll(): Flow<List<Device>>
 
-    @Query("SELECT * FROM devices WHERE macAddress IN (:macAddresses)")
-    suspend fun loadAllByMacAddresses(macAddresses: IntArray): List<Device>
+    @Query("SELECT * FROM devices WHERE id IN (:ids)")
+    suspend fun getInstancesByIds(ids: List<String>): List<Device>
 
-    @Query("SELECT * FROM devices WHERE macAddress LIKE :macAddress LIMIT 1")
-    suspend fun findByMacAddress(macAddress: String): Device
-
-    @Query("SELECT COUNT() FROM devices")
-    fun observeDeviceCount(): Flow<Int>
+    @Query("SELECT * FROM devices WHERE id LIKE :id LIMIT 1")
+    suspend fun getInstanceById(id: String): Device
 
     @Query("SELECT COUNT() FROM devices")
-    suspend fun getDeviceCount(): Int
+    fun observeInstanceCount(): Flow<Int>
 
-    @Insert(onConflict = REPLACE)
-    suspend fun insertAll(vararg devices: Device)
-
-    @Insert(onConflict = REPLACE)
-    suspend fun insert(device: Device)
-
-    @Update
-    suspend fun update(device: Device)
-
-    @Delete
-    suspend fun delete(device: Device)
-
-    @Delete
-    suspend fun delete(vararg devices: Device)
+    @Query("SELECT COUNT() FROM devices")
+    suspend fun getInstanceCount(): Int
 
     @Query("DELETE FROM devices")
     suspend fun deleteAll()
-
 }
