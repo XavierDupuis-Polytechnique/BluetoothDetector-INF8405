c4f9d704bc6c68844bd80ff26b9304da16751970
XavierDupuis <xavier0978@hotmail.fr>
2023-04-18 14:45:48 -0400

    account page strings

--- Projet/app/src/main/java/com/example/bluetoothdetector/auth/view/AccountScreen.kt
+++ Projet/app/src/main/java/com/example/bluetoothdetector/auth/view/AccountScreen.kt
@@ -3,7 +3,9 @@ package com.example.bluetoothdetector.auth.view
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavHostController
+import com.example.bluetoothdetector.R
 import com.example.bluetoothdetector.auth.domain.AccountRedirection
 import com.example.bluetoothdetector.auth.viewmodel.AuthViewModel
 import com.example.bluetoothdetector.auth.viewmodel.removeEmail
@@ -38,17 +40,18 @@ fun AccountScreen(
 
 @Composable
 fun WelcomeBackView(firebaseUser: FirebaseUser) {
-    Subtitle("Welcome back ${firebaseUser.email?.removeEmail()} !")
+    val username = firebaseUser.email?.removeEmail() ?: ""
+    Subtitle(stringResource(R.string.auth_welcome_back, username))
 }
 
 @Composable
 fun LastSignInView(firebaseUser: FirebaseUser) {
-    val date = firebaseUser.metadata?.lastSignInTimestamp?.let { Date(it).formatDate() }
-    Text("Last sign in : $date")
+    val date = firebaseUser.metadata?.lastSignInTimestamp?.let { Date(it).formatDate() } ?: ""
+    Text(stringResource(R.string.auth_last_sign_in, date))
 }
 
 @Composable
 fun AccountCreatedView(firebaseUser: FirebaseUser) {
-    val date = firebaseUser.metadata?.creationTimestamp?.let { Date(it).formatDate() }
-    Text("Account created : $date")
+    val date = firebaseUser.metadata?.creationTimestamp?.let { Date(it).formatDate() } ?: ""
+    Text(stringResource(R.string.auth_account_created, date))
 }
