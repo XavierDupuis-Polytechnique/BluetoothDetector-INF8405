1dac83242696533f4a597dca5aaf2e9021a52c8a
XavierDupuis <xavier0978@hotmail.fr>
2023-04-18 14:20:11 -0400

    hook on getUserCollection

--- Projet/app/src/main/java/com/example/bluetoothdetector/main/sources/DistributedDeviceSource.kt
+++ Projet/app/src/main/java/com/example/bluetoothdetector/main/sources/DistributedDeviceSource.kt
@@ -1,80 +1,60 @@
 package com.example.bluetoothdetector.main.sources
 
-import com.example.bluetoothdetector.auth.repository.AuthRepository
+import com.example.bluetoothdetector.auth.repository.AccountRepository
 import com.example.bluetoothdetector.main.model.Device
 import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.FirebaseFirestoreException
 import com.google.firebase.firestore.SetOptions
 import com.google.firebase.firestore.ktx.toObject
 import com.google.firebase.firestore.ktx.toObjects
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.Job
 import kotlinx.coroutines.awaitAll
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.tasks.asDeferred
 import kotlinx.coroutines.tasks.await
 import javax.inject.Inject
 
 class DistributedDeviceSource @Inject constructor(
     private val firebaseFirestore: FirebaseFirestore,
-    private val authRepository: AuthRepository
+    private val accountRepository: AccountRepository
 ) : CollectionSource<Device, String> {
 
     companion object {
-        const val UserCollectionName = "USER_COLLECTION"
         const val DeviceCollectionName = "DEVICE_COLLECTION"
     }
 
-//    val devices: Flow<List<Device>>
-//        get() =
-//            authRepository.currentUser!!.flatMpa { user ->
-//                currentCollection(user.id).snapshots().map { snapshot -> snapshot.toObjects() }
-//            }
-
-//        currentCollection()?.addSnapshotListener { value, error ->
-//
-//        }
-
     override fun savedInstancesProvider(caller: (List<Device>) -> Unit) {
-        currentCollection()?.get()?.addOnSuccessListener {
+        deviceCollection()?.get()?.addOnSuccessListener {
             caller(it.toObjects())
         }
     }
 
-    override fun observeInstanceCount(): Flow<Int> {
-        // TODO
-        return flowOf(999)
-    }
-
     override suspend fun getAll(): List<Device> {
-        // TODO
-        return listOf()
+        var allSavedDevices = listOf<Device>()
+        deviceCollection()?.get()?.addOnSuccessListener {
+            allSavedDevices = it.toObjects()
+        }?.await()
+        return allSavedDevices
     }
 
     override suspend fun get(id: String): Device? {
-        return currentCollection()?.document(id)?.get()?.await()?.toObject()
+        return deviceCollection()?.document(id)?.get()?.await()?.toObject()
     }
 
     override suspend fun delete(instance: Device) {
-        currentCollection()?.document(instance.macAddress)?.delete()?.await()
+        deviceCollection()?.document(instance.id)?.delete()?.await()
     }
 
     override suspend fun deleteAll() {
-        currentCollection()?.get()?.await()?.map { it.reference.delete().asDeferred() }?.awaitAll()
+        deviceCollection()?.get()?.await()?.map { it.reference.delete().asDeferred() }?.awaitAll()
     }
 
     override suspend fun insert(instance: Device) {
-        currentCollection()?.document(instance.macAddress)?.set(instance, SetOptions.merge())
+        deviceCollection()?.document(instance.id)?.set(instance, SetOptions.merge())
     }
 
-    private fun currentCollection(uid: String? = authRepository.userId): CollectionReference? =
-        uid?.let {
-            firebaseFirestore
-                .collection(UserCollectionName)
-                .document(it)
-                .collection(DeviceCollectionName)
-        }
+    private fun deviceCollection(
+        uid: String? = accountRepository.currentUserId
+    ): CollectionReference? =
+        uid?.let { accountRepository.getUserCollection(uid).collection(DeviceCollectionName) }
 
+    val onDeviceCollectionChange = accountRepository.currentUser
 }
