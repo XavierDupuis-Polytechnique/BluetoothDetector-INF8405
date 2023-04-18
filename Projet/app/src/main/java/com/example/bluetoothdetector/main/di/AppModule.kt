21991ba975b087f2b6e3a6995eeadf3abc6c66bf
XavierDupuis <xavier0978@hotmail.fr>
2023-04-18 14:15:55 -0400

    Update AppModule.kt

--- Projet/app/src/main/java/com/example/bluetoothdetector/main/di/AppModule.kt
+++ Projet/app/src/main/java/com/example/bluetoothdetector/main/di/AppModule.kt
@@ -2,13 +2,13 @@ package com.example.bluetoothdetector.main.di
 
 import android.content.Context
 import androidx.room.Room
-import com.example.bluetoothdetector.auth.repository.AuthRepository
+import com.example.bluetoothdetector.auth.repository.AccountRepository
 import com.example.bluetoothdetector.main.repository.BluetoothRepository
 import com.example.bluetoothdetector.main.repository.DeviceRepository
 import com.example.bluetoothdetector.main.repository.LocationRepository
 import com.example.bluetoothdetector.main.repository.NetworkRepository
-import com.example.bluetoothdetector.main.sources.LocalDeviceSource
 import com.example.bluetoothdetector.main.sources.DistributedDeviceSource
+import com.example.bluetoothdetector.main.sources.LocalDeviceSource
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import com.google.firebase.firestore.FirebaseFirestore
@@ -89,7 +89,7 @@ object AppModule {
     @Provides
     fun provideDistributedDeviceSource(
         firebaseFirestore: FirebaseFirestore,
-        authRepository: AuthRepository
-    ) = DistributedDeviceSource(firebaseFirestore, authRepository)
+        accountRepository: AccountRepository
+    ) = DistributedDeviceSource(firebaseFirestore, accountRepository)
 }
 
