709649a87060e38e01e15d2b61d389b3478f2f26
medimad31 <122495066+medimad31@users.noreply.github.com>
2023-04-19 17:19:34 -0500

    camerax

--- Projet/app/src/main/java/com/example/bluetoothdetector/MainActivity.kt
+++ Projet/app/src/main/java/com/example/bluetoothdetector/MainActivity.kt
@@ -5,15 +5,20 @@ import android.bluetooth.BluetoothAdapter.ACTION_DISCOVERY_FINISHED
 import android.bluetooth.BluetoothDevice
 import android.content.IntentFilter
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.os.LocaleListCompat
 import com.example.bluetoothdetector.common.domain.LanguageStateSaver
 import com.example.bluetoothdetector.common.repository.LanguageRepository
@@ -24,9 +29,15 @@ import com.example.bluetoothdetector.main.repository.BluetoothRepository
 import com.example.bluetoothdetector.main.repository.LocationRepository
 import com.example.bluetoothdetector.main.repository.NetworkRepository
 import com.example.bluetoothdetector.ui.theme.BluetoothDetectorTheme
+import com.example.bluetoothdetector.ui.theme.CameraView
 import dagger.hilt.android.AndroidEntryPoint
+import java.io.File
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 import javax.inject.Inject
-
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
+import com.google.firebase.FirebaseApp
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -46,10 +57,66 @@ class MainActivity : AppCompatActivity() {
     @Inject
     lateinit var networkRepository: NetworkRepository
 
+
+
+
+    private lateinit var outputDirectory: File
+    private lateinit var cameraExecutor: ExecutorService
+
+    private var shouldShowCamera: MutableState<Boolean> = mutableStateOf(false)
+
+    private lateinit var photoUri: Uri
+    private var shouldShowPhoto: MutableState<Boolean> = mutableStateOf(false)
+
+
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ){isGranted ->
+        if (isGranted){
+            Log.i("Kilo", "Pemission granted")
+            shouldShowCamera.value = true
+        }else{
+            Log.i("Kilo", "Permission denied")
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        FirebaseApp.initializeApp(this)
         super.onCreate(savedInstanceState)
         setContent {
             MainContent(themeRepository, languageRepository)
+
+            if (shouldShowCamera.value) {
+                CameraView(
+                    outputDirectory = outputDirectory,
+                    executor = cameraExecutor,
+                    onImageCaptured = ::handleImageCapture,
+                    onError = { Log.e("kilo", "View error:", it) }
+                )
+            }
+
+            if (shouldShowPhoto.value) {
+                Image(
+                    painter = rememberImagePainter(photoUri),
+                    contentDescription = null,
+                    modifier = Modifier.fillMaxSize()
+                )
+            }
+
+
         }
         bluetoothRepository.bluetoothStarted = false
         startBTScan()
@@ -61,7 +128,66 @@ class MainActivity : AppCompatActivity() {
                 )
             }
         networkRepository.updateCreatedBytes()
+
+
+
+
+
+        requestCameraPermission()
+
+        outputDirectory = getOutputDirectory()
+        cameraExecutor = Executors.newSingleThreadExecutor()
+
+
     }
+
+    private fun Image(painter: Unit, contentDescription: Nothing?, modifier: Modifier) {
+
+    }
+
+    private fun rememberImagePainter(photoUri: Uri) {
+
+    }
+
+
+    private fun requestCameraPermission(){
+        when{
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CAMERA
+            ) == PackageManager.PERMISSION_GRANTED ->{
+                Log.i("Kilo", "Permission previously granted")
+                shouldShowCamera.value = true
+            }
+            ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                Manifest.permission.CAMERA
+            ) -> Log.i("Kilo", "Show camera permissions dialog")
+
+            else -> requestPermissionLauncher.launch(Manifest.permission.CAMERA)
+        }}
+
+        private fun handleImageCapture(uri: Uri) {
+            Log.i("kilo", "Image captured: $uri")
+            shouldShowCamera.value = false
+
+            photoUri = uri
+            shouldShowPhoto.value = true
+        }
+
+        private fun getOutputDirectory(): File {
+            val mediaDir = externalMediaDirs.firstOrNull()?.let {
+                File(it, resources.getString(R.string.app_name)).apply { mkdirs() }
+            }
+
+            return if (mediaDir != null && mediaDir.exists()) mediaDir else filesDir
+        }
+
+
+
+
+
+
 
     override fun onResume() {
         super.onResume()
@@ -92,6 +218,8 @@ class MainActivity : AppCompatActivity() {
         if (bluetoothRepository.bluetoothReceiver != null) {
             unregisterReceiver(bluetoothRepository.bluetoothReceiver)
         }
+        super.onDestroy()
+        cameraExecutor.shutdown()
     }
 
     // Start bluetooth scan
@@ -168,4 +296,8 @@ fun MainContent(
             permissionsViewModel
         )
     }
+
+
+
+
 }
