e1e1f22af2db33ddfa336223cb9c0361cb5d967d
XavierDupuis <xavier0978@hotmail.fr>
2023-04-19 10:26:01 -0400

    image picker strings

--- Projet/app/src/main/java/com/example/bluetoothdetector/common/view/camera/ImagePicker.kt
+++ Projet/app/src/main/java/com/example/bluetoothdetector/common/view/camera/ImagePicker.kt
@@ -3,21 +3,14 @@ package com.example.bluetoothdetector.common.view.camera
 import android.net.Uri
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
-import coil.compose.AsyncImage
+import com.example.bluetoothdetector.R
 import com.example.bluetoothdetector.common.view.containers.CardContainer
 import com.example.bluetoothdetector.common.view.containers.CenteredHorizontalContainer
 import com.example.bluetoothdetector.common.view.containers.CenteredVerticalContainer
@@ -27,7 +20,7 @@ import com.example.bluetoothdetector.common.viewmodel.ImagePickerViewModel
 fun ImagePicker(
     displayedImageUri: MutableState<Uri?>,
     viewModel: ImagePickerViewModel = hiltViewModel(),
-    imageContent: @Composable (Uri) -> Unit = { ImagePreview(it) }
+    imageContent: @Composable (Uri) -> Unit = { ImageView(it) }
 ) {
     val pickImageLauncher = rememberLauncherForActivityResult(
         contract = ActivityResultContracts.GetContent(),
@@ -52,26 +45,12 @@ fun ImagePicker(
         }
         CenteredVerticalContainer {
             Button(onClick = { viewModel.pickImage(pickImageLauncher) }) {
-                Text("Select Image")
+                Text(stringResource(R.string.select_image))
             }
             Button(onClick = { viewModel.takePicture(context, takePictureLauncher) }) {
-                Text("Take photo")
+                Text(stringResource(R.string.take_picture))
             }
         }
     }
-}
-
-@Composable
-fun ImagePreview(uri: Uri) {
-    AsyncImage(
-        model = uri,
-        contentDescription = uri.toString(),
-        contentScale = ContentScale.Crop,
-        modifier = Modifier
-            .padding(top = 64.dp)
-            .clip(CircleShape)
-            .width(128.dp)
-            .height(128.dp)
-    )
 }
 
